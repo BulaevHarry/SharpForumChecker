22bb91eccbd7fae48cb764c6e131cc1c1e132c9c
Asazeus <contacts.asazeus@gmail.com>
2013-11-04 03:40:58 +0200

    Добавлены комментарии и внесены мелкие исправления

---
+++ SharpForumChecker/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace SharpForumChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBarSearch = new System.Windows.Forms.ProgressBar();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonRemoveWord = new System.Windows.Forms.Button();
+            this.buttonAddWord = new System.Windows.Forms.Button();
+            this.listBoxWords = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkedListBoxSections = new System.Windows.Forms.CheckedListBox();
+            this.timerSearch = new System.Windows.Forms.Timer(this.components);
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSearch.Location = new System.Drawing.Point(3, 386);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(192, 32);
+            this.buttonSearch.TabIndex = 0;
+            this.buttonSearch.Text = "Искать";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.Location = new System.Drawing.Point(202, 2);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(288, 420);
+            this.listBox1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.progressBarSearch);
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Controls.Add(this.buttonSearch);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 423);
+            this.panel1.TabIndex = 4;
+            // 
+            // progressBarSearch
+            // 
+            this.progressBarSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.progressBarSearch.Location = new System.Drawing.Point(3, 357);
+            this.progressBarSearch.Maximum = 60;
+            this.progressBarSearch.Name = "progressBarSearch";
+            this.progressBarSearch.Size = new System.Drawing.Size(192, 23);
+            this.progressBarSearch.Step = 1;
+            this.progressBarSearch.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarSearch.TabIndex = 3;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.checkBox3);
+            this.groupBox3.Controls.Add(this.checkBox2);
+            this.groupBox3.Controls.Add(this.checkBox1);
+            this.groupBox3.Location = new System.Drawing.Point(3, 290);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(192, 62);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Если нашел:";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(6, 44);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(102, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "еще что-то там";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(6, 29);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(129, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Открыть в браузере";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 14);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(50, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Звук";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonRemoveWord);
+            this.groupBox2.Controls.Add(this.buttonAddWord);
+            this.groupBox2.Controls.Add(this.listBoxWords);
+            this.groupBox2.Location = new System.Drawing.Point(3, 135);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(192, 155);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ключевые слова";
+            // 
+            // buttonRemoveWord
+            // 
+            this.buttonRemoveWord.Location = new System.Drawing.Point(100, 121);
+            this.buttonRemoveWord.Name = "buttonRemoveWord";
+            this.buttonRemoveWord.Size = new System.Drawing.Size(85, 23);
+            this.buttonRemoveWord.TabIndex = 2;
+            this.buttonRemoveWord.Text = "Удалить";
+            this.buttonRemoveWord.UseVisualStyleBackColor = true;
+            this.buttonRemoveWord.Click += new System.EventHandler(this.buttonRemoveWord_Click);
+            // 
+            // buttonAddWord
+            // 
+            this.buttonAddWord.Location = new System.Drawing.Point(9, 121);
+            this.buttonAddWord.Name = "buttonAddWord";
+            this.buttonAddWord.Size = new System.Drawing.Size(85, 23);
+            this.buttonAddWord.TabIndex = 1;
+            this.buttonAddWord.Text = "Добавить";
+            this.buttonAddWord.UseVisualStyleBackColor = true;
+            this.buttonAddWord.Click += new System.EventHandler(this.buttonAddWord_Click);
+            // 
+            // listBoxWords
+            // 
+            this.listBoxWords.FormattingEnabled = true;
+            this.listBoxWords.Location = new System.Drawing.Point(8, 19);
+            this.listBoxWords.Name = "listBoxWords";
+            this.listBoxWords.Size = new System.Drawing.Size(178, 95);
+            this.listBoxWords.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkedListBoxSections);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 130);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Искать в разделах:";
+            // 
+            // checkedListBoxSections
+            // 
+            this.checkedListBoxSections.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkedListBoxSections.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.checkedListBoxSections.CheckOnClick = true;
+            this.checkedListBoxSections.FormattingEnabled = true;
+            this.checkedListBoxSections.Items.AddRange(new object[] {
+            "Авто, мото, вело",
+            "ПК, комплект., оргтехника",
+            "Фото и видео",
+            "Электроника, бытовая техника",
+            "Связь, мобильные и смартфоны",
+            "Одежда и аксессуары",
+            "Разное"});
+            this.checkedListBoxSections.Location = new System.Drawing.Point(6, 18);
+            this.checkedListBoxSections.Name = "checkedListBoxSections";
+            this.checkedListBoxSections.Size = new System.Drawing.Size(185, 105);
+            this.checkedListBoxSections.TabIndex = 1;
+            this.checkedListBoxSections.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // timerSearch
+            // 
+            this.timerSearch.Interval = 1000;
+            this.timerSearch.Tick += new System.EventHandler(this.timerSearch_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 423);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Монитор барахолки нулевого дня";
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBoxSections;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ProgressBar progressBarSearch;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button buttonRemoveWord;
+        private System.Windows.Forms.Button buttonAddWord;
+        private System.Windows.Forms.ListBox listBoxWords;
+        private System.Windows.Forms.Timer timerSearch;
+    }
+}
+
---
+++ SharpForumChecker/Resources/Checker0day.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using HtmlAgilityPack;
+using System.Media;
+
+namespace SharpForumChecker.Resources
+{
+    class Checker0day
+    {
+        public Resources.Sections sections; //ссилки на сайти, де шукаємо
+        public List<string> keyWords; //список ключових слов
+        public List<string> founded;  //список всього найденого (назви тем)
+        private List<string> blackList; //"чорний список" куди заносяться останні цифри в ссилкі на тему
+
+        System.IO.Stream SoundStream; //для витягування вавки з ресурсів
+        private SoundPlayer soundP;   //для проігрування цеї вавки
+
+        public bool ActBrowser; //настройки реакції на знайдене
+        public bool ActBeep;
+        
+        public Checker0day() {          //ініціалізація
+            sections = new Resources.Sections();
+            keyWords = new List<string>();
+            founded = new List<string>();
+            ActBeep = true;
+            blackList = new List<string>();
+            SoundStream = Properties.Resources.alert;
+            soundP = new SoundPlayer(SoundStream);
+        }
+
+        [DllImport("shell32.dll")] //для відкриття сторінки в браузері через winapi, було впадляк гуглить як воно у вас, шарперів, робиться
+        private static extern int ShellExecute(int hWnd, string Operation, string File, string Parameters, string Directory, int nShowCmd);
+
+        public bool Check()
+        {
+            bool result = false; 
+
+            for (int i = 0; i < 7; i++)
+            {
+                if (sections.GetIndex(i))
+                {
+                    if (CheckIt(sections.GetSite(i)))
+                    {
+                        result = true; //якшо хоч раз шось знайшлося
+                    }
+                    
+                }
+            }
+
+            if (result && ActBeep)
+            {
+                soundP.Play();
+            }
+
+            return result;
+        }
+
+        private bool CheckIt(string site){
+            bool res = false;
+
+            HtmlAgilityPack.HtmlDocument DOC = new HtmlAgilityPack.HtmlDocument(); //я хз треба чи не треба кожен раз пересоздавать цей екзкмпляр. шо ти думаєш, Круел?
+            var web = new HtmlWeb
+            {
+                AutoDetectEncoding = false,
+                OverrideEncoding = System.Text.ASCIIEncoding.GetEncoding(1251), //дякую за цю подсказку, Круел.
+            };
+
+            DOC = web.Load(site);
+            if (DOC == null)
+            {
+                return false; 
+            }
+
+
+            //і тут я починаю розгрібать html'ку.. розбирав як умію, якшо є кращий спосіб - крітікуй
+            var trList = DOC.DocumentNode.SelectNodes("//tr");
+            foreach (var tr in trList) //забавна штука цей форіч
+            {
+               var tdList = tr.ChildNodes.Where(x => x.Name == "td");
+               foreach (var td in tdList)
+               {
+                   var divList = td.ChildNodes.Where(x => x.Name == "div");
+                   foreach (var div in divList)
+                   {
+                        var spanList = div.ChildNodes.Where(x => x.Name == "span"); //тут у нас в тексті назва теми
+                        foreach (var span in spanList)
+                        {
+                              if (span.InnerHtml.Contains("Тема создана:")) //а так я вирішив виділить тільки спани з темами
+                              {
+                                  foreach (string word in keyWords) //перевіряю чи є ключові слова в назві теми
+                                  {
+                                      if (span.InnerText.Contains(word))
+                                      {
+                                          var aList = span.ChildNodes.Where(x => x.Name == "a"); //витягую номер топіка
+                                          foreach (var a in aList)
+                                          {
+                                              string temp_str = a.Attributes["href"].Value; //тут якась не дуже робоча ссилка в кінці якої наш номер
+                                              string target_str = "";
+
+                                              for (int ii = temp_str.LastIndexOf('=') + 1; ii < temp_str.Length; ii++)
+                                              {
+                                                  target_str += temp_str[ii].ToString();
+                                              } //а тепер в таргет_стр наш номер топіка
+
+                                              if (blackList.Contains(target_str) == false) //якшо ще не реагував на такий номер топіка
+                                              {
+                                                  founded.Add(span.InnerText/*.Trim()*/);
+                                                  res = true;//наче як все нормально, а значить в кінці вернемо "правду"
+
+                                                  if (ActBrowser == true) ShellExecute(0, "open", "http://forum.0day.kiev.ua/index.php?showtopic=" + target_str, "", "", 1);//открить в браузері
+
+                                                  blackList.Add(target_str); //реагую і добавляю в блек ліст
+                                              }
+
+                                          }
+                                      }
+                                  }
+                              }
+                        }
+                   }
+               }
+            }
+
+            return res;
+        }
+        
+    }
+}
